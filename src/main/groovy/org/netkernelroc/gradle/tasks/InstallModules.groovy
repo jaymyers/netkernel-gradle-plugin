e2f4ea81f7c1df81bf7b5364f64a1f3c040ebae9
Randolph Kahle <randy@databliss.net>
2013-02-05 08:51:32 -0700

    Fix argument name spelling error

---
+++ src/main/groovy/org/netkernelroc/gradle/tasks/InstallModules.groovy
@@ -0,0 +1,11 @@
+package org.netkernelroc.gradle.tasks
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: randolph.kahle
+ * Date: 2/5/13
+ * Time: 8:42 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class InstallModules {
+}
