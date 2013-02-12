6752c1c675deddf42a93a812c195309a0306367f
Randolph Kahle <randy@databliss.net>
2013-02-12 13:56:47 -0700

    Cleanup

--- src/main/groovy/org/netkernelroc/gradle/NetKernelPlugin.groovy
+++ src/main/groovy/org/netkernelroc/gradle/NetKernelPlugin.groovy
@@ -14,45 +14,44 @@ import org.netkernelroc.gradle.util.NetKernelHelper
  */
 class NetKernelPlugin implements Plugin<Project> {
 
-    void apply(Project p) {
-        println "You are applying $p.name"
+  void apply(Project p) {
+    println "You are applying $p.name"
 
-        p.extensions.create("fsHelper", FileSystemHelper)
-        p.extensions.create("nkHelper", NetKernelHelper)
+    p.extensions.create("fsHelper", FileSystemHelper)
+    p.extensions.create("nkHelper", NetKernelHelper)
 
-        p.tasks.add(name: 'downloadNetKernel', type: DownloadNKSE)
-        p.tasks.add(name: 'netkernelStatus', type: NetKernelStatus)
-        p.tasks.add(name: 'installNetKernelModules', type: InstallModules)
-        p.tasks.add(name: 'removeNetKernelModules', type: RemoveModules)
+    p.tasks.add(name: 'downloadNetKernel', type: DownloadNKSE)
+    p.tasks.add(name: 'statusNetKernel', type: NetKernelStatus, description: "Reports on the status of the currently running NetKernel.")
+    p.tasks.add(name: 'installNetKernelModules', type: InstallModules, description: "Installs modules in the currently running NetKernel.")
+    p.tasks.add(name: 'removeNetKernelModules', type: RemoveModules, description: "Removes modules from the currently running NetKernel.")
+    p.tasks.add(name: 'removeAllNetKernelModules', type: RemoveAllModules, description: "Removes all modules from currently running NetKernel.")
 
-        p.tasks.add(name: 'runNetKernelJar', type: RunNKSEJar) {
-            workingDir "${project.fsHelper.gradleHomeDir()}/netkernel"
-            commandLine '/usr/bin/java', '-jar', "${project.fsHelper.gradleHomeDir()}/downloads/1060-NetKernel-SE-5.1.1.jar"
+    p.tasks.add(name: 'runNetKernelJar', type: RunNKSEJar) {
+      workingDir "${project.fsHelper.gradleHomeDir()}/netkernel"
+      commandLine '/usr/bin/java', '-jar', "${project.fsHelper.gradleHomeDir()}/downloads/1060-NetKernel-SE-5.1.1.jar"
 
-            onlyIf { !project.nkHelper.isNetKernelRunning() }
-        }
-
-        p.getGradle().getTaskGraph().addTaskExecutionListener( new TaskExecutionListener() {
-            @Override
-            void beforeExecute(Task task) {
-                if(task.name == 'installNetKernelJar') {
-                   /* def es = Executors.newSingleThreadExecutor()
-                    def f = es.submit({ p.tasks.runNetKernelJar.execute()} as Callable)
-                    println f.get()
-                    println "HUZZAH" */
-                }
-            }
-
-            @Override
-            void afterExecute(Task task, TaskState taskState) {
-               // println task.name
-            }
-        })
-
-        p.tasks.add(name: 'installNetKernelJar', type: InstallNKSEJar) {
-            onlyIf { !project.fsHelper.gradleHomeDirectoryExists("/netkernel/install") }
-        }
-
-        p.tasks.installNetKernelJar.dependsOn "downloadNetKernel"
+      onlyIf { !project.nkHelper.isNetKernelRunning() }
     }
+
+    p.getGradle().getTaskGraph().addTaskExecutionListener(new TaskExecutionListener() {
+      @Override void beforeExecute(Task task) {
+        if (task.name == 'installNetKernelJar') {
+          /* def es = Executors.newSingleThreadExecutor()
+           def f = es.submit({ p.tasks.runNetKernelJar.execute()} as Callable)
+           println f.get()
+           println "HUZZAH" */
+        }
+      }
+
+      @Override void afterExecute(Task task, TaskState taskState) {
+        // println task.name
+      }
+    })
+
+    p.tasks.add(name: 'installNetKernelJar', type: InstallNKSEJar) {
+      onlyIf { !project.fsHelper.gradleHomeDirectoryExists("/netkernel/install") }
+    }
+
+    p.tasks.installNetKernelJar.dependsOn "downloadNetKernel"
+  }
 }
---
+++ src/main/groovy/org/netkernelroc/gradle/tasks/RemoveAllModules.groovy
@@ -0,0 +1,29 @@
+package org.netkernelroc.gradle.tasks
+
+import org.gradle.api.DefaultTask
+
+class RemoveAllModules extends DefaultTask {
+
+
+  @org.gradle.api.tasks.TaskAction void removeAllNetKernelModules() {
+
+    // Get the directory where this script was executed
+    def projectDirectory = System.getProperty("user.dir")
+    // Get the name of the parent directory to use as the name of the modules.xml file in the modules.d directory
+    def projectName = projectDirectory.split("/").last()
+
+    def String installLocation = project.nkHelper.whereIsNetKernelInstalled()
+    def String moduleExtensionDirectory = project.nkHelper.whereIsModuleExtensionDirectory()
+    def netkernelRunning = true
+
+    if (netkernelRunning) {
+      File modulesD = new File(installLocation + moduleExtensionDirectory)
+      modulesD.listFiles().each { File file ->
+        if (!file.isDirectory()){
+        println "Removing modules defined in ${file.name}"
+        file.delete()
+        }
+      }
+    }
+  }
+}
