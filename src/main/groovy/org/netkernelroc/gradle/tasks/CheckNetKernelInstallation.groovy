50baa5a03e61994897883c1279e3de1b4bf79870
Randolph Kahle <randy@databliss.net>
2013-02-25 12:50:47 -0700

    Minor cleanup

--- src/main/groovy/org/netkernelroc/gradle/tasks/CheckNetKernelInstallation.groovy
+++ src/main/groovy/org/netkernelroc/gradle/tasks/CheckNetKernelInstallation.groovy
@@ -1,22 +1,16 @@
-package groovy.org.netkernelroc.gradle.tasks
+package org.netkernelroc.gradle.tasks
 
 import org.gradle.api.DefaultTask
 
-/**
- * Created with IntelliJ IDEA.
- * User: brian
- * Date: 2/1/13
- * Time: 8:34 PM
- * To change this template use File | Settings | File Templates.
- */
+
 class CheckNetKernelInstallation extends DefaultTask {
 
-    @org.gradle.api.tasks.TaskAction
-    def checkInstallation() {
-        if(project.nkHelper.isNetKernelInstalled()) {
-            // I know about THESE NetKernel instances
-        } else {
-            println "NetKernel is not installed."
-        }
+  @org.gradle.api.tasks.TaskAction
+  def checkInstallation() {
+    if (project.nkHelper.isNetKernelInstalled()) {
+      // I know about THESE NetKernel instances
+    } else {
+      println "NetKernel is not installed."
     }
+  }
 }
